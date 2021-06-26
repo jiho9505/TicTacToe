3d100cb5ecc79117a2ba838868e6e9aff245b323
jiho9505 <sun_days@naver.com>
2021-06-26 18:21:04 +0900

    Finish Counter

---
+++ src/components/Counter/Counter.js
@@ -0,0 +1,39 @@
+export default class Counter {
+
+    left = 0;
+    right = 0;
+  
+    constructor(score) {
+      this.left = score.left
+      this.right = score.right
+
+      const TEMPLATE = document.createElement("template");
+      
+      TEMPLATE.innerHTML = `
+              <div class='counter__part'>  
+              </div>
+              `
+  
+      this.element = TEMPLATE.content.firstElementChild;
+
+      this.render()
+    }
+  
+    attachTo(parent) {
+      parent.appendChild(this.element)
+    }
+  
+    setState(newScore) {
+        this.left = newScore.left
+        this.right = newScore.right
+        this.render();
+    }
+
+    render() {
+        this.element.innerHTML = `
+            <span>${this.left}</span>
+            <span>:</span>
+            <span>${this.right}</span>
+        `
+    }
+  }
