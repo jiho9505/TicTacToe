7e9ed5d41f4b0ef58017e8964bf71e63bdf0d5f3
jiho9505 <sun_days@naver.com>
2021-06-26 18:22:47 +0900

    Create App Frame

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import Counter from './components/Counter/Counter.js'
+import Footer from './components/Footer/Footer.js'
+// import Tictactoe from './components/Tictactoe/Tictactoe.js'
+
+export default class App {
+  $target = null;
+  score = {
+      left : 0,
+      right : 0
+  };
+  datas = Array.from({length : 3}, () => Array(3).fill(0));
+
+  constructor($target) {
+    this.$target = $target;
+
+    this.counter = new Counter(this.score);
+    this.counter.attachTo($target)
+
+    // this.tictactoe = new Tictactoe();
+    // this.tictactoe.attachTo($target);
+
+    this.footer = new Footer({
+        onClick: (type) => {
+            if(type === 'New'){
+                this.initData()
+                this.setState()
+            }else{
+                this.initCount()
+                this.initData()
+                this.setState()
+            }
+
+        }
+    });
+    this.footer.attachTo($target);
+
+  }
+
+  initCount() {
+    this.score.left = 0
+    this.score.right = 0
+  }
+  
+  initData() {
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            this.datas[i][j] = 0;
+        }
+    }
+  }
+  
+  setState() {
+    this.counter.setState(this.score)
+    // this.tictactoe.setState(this.datas)
+  }
+
+}
